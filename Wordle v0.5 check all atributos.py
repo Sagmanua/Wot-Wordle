084cb658923724e0f0b9ff3a9f8eb0ff280ef9f9
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-23 17:58:47 +0100

    Create Wordle v0.5 check all atributos.py

---
+++ Wordle v0.5 check all atributos.py
@@ -0,0 +1,174 @@
+import random
+worlds = [
+        {
+            "name": "e100",
+            "Class": "Heavy",
+            "damage":"520",
+            "speed":"slow"
+            
+        },
+        {
+            "name":"manticora",
+            "Class": "Light",
+            "damage":"390",
+            "speed":"fast"
+        },
+        {
+            "name": "Maus",
+            "Class": "Heavy",
+            "damage":"520",
+            "speed":"slow"
+            
+        },
+        {
+            "name": "Ebr",
+            "Class": "Light",
+            "damage":"390",
+            "speed":"superfast"
+            
+        },
+        {
+            "name": "Is-7",
+            "Class": "Heavy",
+            "damage":"490",
+            "speed":"medium"
+            
+        },
+        {
+            "name": "T-100lt",
+            "Class": "Light",
+            "damage":"320",
+            "speed":"fast"
+            
+        }
+]
+
+#Declara variables
+tryes = 0
+
+#### Take random tank 
+nummber = random.randrange(len(worlds))
+tank_of_guess = worlds[nummber]["name"]
+print(tank_of_guess)
+
+# Speed ranks 
+speed_rank = {
+    "veryslow": 1,
+    "slow": 2,
+    "medium": 3,
+    "fast": 4,
+    "superfast": 5
+}
+
+
+
+def check_on_write_enter ():
+    for world in worlds:
+        if world["name"] == name_guess:
+            return True
+    return False
+
+
+def show_all_names ():
+        for i,contacto  in enumerate(worlds):
+            print(i ,contacto["name"])
+
+
+
+
+
+#---------------------Check all atributos that have tanks 
+#### Check for atrtributos in random tank
+def get_attr(name_guess, attr):
+    for world in worlds:
+        if world["name"] == name_guess:
+            return world.get(attr)  # безопасно вернёт None если атрибута нет
+    return None
+
+def get_attr_of_random_tank(tank_of_guess, attr):
+    for world in worlds:
+        if world["name"] == tank_of_guess:
+            return world.get(attr)  # безопасно вернёт None если атрибута нет
+    return None
+
+damage_random_tank = get_attr_of_random_tank(tank_of_guess, "damage")
+speed_random_tank = get_attr_of_random_tank(tank_of_guess, "speed")
+class_random_tank = get_attr_of_random_tank(tank_of_guess, "Class")
+print(damage_random_tank,speed_random_tank,class_random_tank)
+
+#--------------------Check atributos 
+### Check bu damage
+def Check_atributos_damage (damage_random_tank,damage_guess):
+    if damage_random_tank == damage_guess:
+        return("Damage the same")
+    elif damage_random_tank>damage_guess:
+        return("Damage is lower")
+    else:
+        return("Damage is bigger")
+### Check by class
+def Check_atributos_class (class_random_tank,class_guess):
+    if class_guess == class_random_tank:
+        return ("This is",class_guess)
+    else:
+        return("This is not",class_guess)
+###Chaeck speed
+def Check_atributos_Speed(speed_random_tank, speed_guess):
+    s1 = speed_rank[speed_random_tank.lower()]
+    s2 = speed_rank[speed_guess.lower()]
+
+    if s1 > s2:
+        return "Random tank is faster"
+    elif s1 < s2:
+        return "Random tank is slower"
+    else:
+        return "It has the same speed"
+
+
+print("Hello in my app")
+print("This is world for Wot")
+print("Try to guess what is guess today")
+print("This is list of all tanks")
+show_all_names ()
+
+
+
+
+
+
+while True:
+    #### input of user 
+    name_guess = input("Write tank name")
+    
+    ### Check guess of user
+    if check_on_write_enter():
+        print("Name is Right wirte")
+    else:
+        print("Write again")
+        pass
+    
+    # check is user write all write
+    tryes = tryes + 1 
+    print("This is your",tryes,"try")
+
+    ### Check for atibutos that have chosen tank of user 
+    damage_guess = get_attr(name_guess, "damage")
+    speed_guess = get_attr(name_guess, "speed")
+    class_guess = get_attr(name_guess,"Class")
+    print("Damage of tank that you guess",damage_guess)
+    print("Speed of tank that you guess",speed_guess)
+    print("Speed of tank that you guess",class_guess)
+
+    print(Check_atributos_damage(damage_guess,damage_random_tank))
+    print(Check_atributos_class(class_guess,class_random_tank))
+    print(Check_atributos_Speed(speed_guess,speed_random_tank))
+    # Is a guess or not 
+    if name_guess != tank_of_guess:
+        print("This is not this answer ")
+        
+    else:
+        print("You win")
+        break
+
+
+
+    
