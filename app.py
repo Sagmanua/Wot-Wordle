60ba26a8c2682578ceeebba03a70123a05ca6fa4
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-23 16:08:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -82,18 +82,23 @@ atrtributos_guess = atrtributos_gue(tank_of_guess)
 print(atrtributos_guess)
 
 
-
+print("Hello in my app")
 while True:
     #### input of user 
     name_guess = input("Write tank name")
-    # check is user write all write
-    tryes = tryes + 1 
-    print("This is your",tryes,"try")
+    
+    
     if check_on_write_enter == True:
         print("NAme is Right wirte")
     else:
         print("Write again")
-    
+        pass
+
+    # check is user write all write
+    tryes = tryes + 1 
+    print("This is your",tryes,"try")
+
+
     # Is a guess or not 
     if name_guess != tank_of_guess:
         print("This is not this answer ")
