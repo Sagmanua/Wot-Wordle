d00117acdfe86ac28a40d3130d42260d59acc695
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-11-23 16:03:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,23 +42,64 @@ worlds = [
             
         }
 ]
+
+#Declara variables
+tryes = 0
+
+#### Take random tank 
 nummber = random.randrange(len(worlds))
-print(nummber)
-name_guess = "e100"
+tank_of_guess = worlds[nummber]["name"]
+print(tank_of_guess)
+
+
+
+
+def check_on_write_enter ():
+    for world in worlds:
+        if world["name"] == name_guess:
+            return True
+    return False
+
+
+
+
+### Check guess of user
+
+
+
+
+
+
+
+#### Chech for atrtributos in random tank
 def atrtributos_gue(name_guess):
     for world in worlds:
         if world["name"] == name_guess:
             return world["atrtributos"]   # return the attribute if found
     return None  # if name not found
 
-
-
-
-atrtributos_guess = atrtributos_gue(name_guess)
+atrtributos_guess = atrtributos_gue(tank_of_guess)
 print(atrtributos_guess)
 
 
-if name_guess == worlds[nummber]["name"]:
-    print("true")
-else:
-    print("false")
+
+while True:
+    #### input of user 
+    name_guess = input("Write tank name")
+    # check is user write all write
+    tryes = tryes + 1 
+    print("This is your",tryes,"try")
+    if check_on_write_enter == True:
+        print("NAme is Right wirte")
+    else:
+        print("Write again")
+    
+    # Is a guess or not 
+    if name_guess != tank_of_guess:
+        print("This is not this answer ")
+    else:
+        print("You win")
+
+
+
+    
